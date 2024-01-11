c2cf4254000b5647c168991672a25d52a1bd3075
AlenaPanPanda <guiyguker@gmail.com>
2024-01-11 14:24:54 +0100

    working swipe

--- app/src/main/java/com/example/phrases/Adapter.kt
+++ app/src/main/java/com/example/phrases/Adapter.kt
@@ -8,12 +8,11 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class Adapter(
-    public val dataList: MutableList<QuoteData>
+    val dataList: MutableList<QuoteData>
 ) : RecyclerView.Adapter<Adapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val phrase: TextView = view.findViewById(R.id.phrase_tv)
         val author: TextView = view.findViewById(R.id.author_tv)
-        val deleteButton: ImageView = view.findViewById(R.id.delete_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,14 +27,5 @@ class Adapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.phrase.text = dataList[position].phrase
         holder.author.text = dataList[position].author
-
-        holder.deleteButton.setOnClickListener {
-            removeItem(holder.adapterPosition)
-        }
-    }
-
-    private fun removeItem(position: Int) {
-        dataList.removeAt(position)
-        notifyItemRemoved(position)
     }
 }
